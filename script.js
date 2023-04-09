07861dbb52135cabc2b5a353ed373cc79e142546
FlipFlop222 <bnsari789@gmail.com>
2023-04-09 13:42:35 +0300

    Finish basic calculator, cant handle two operator before equal

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
 let operand1;
 let operand2;
 let operator;
-let display = document.querySelector(".screen");
+let display = document.querySelector(".operand");
+let display2 = document.querySelector(".operand2");
+let display3 = document.querySelector(".operator");
 let button = document.querySelector(".numbers");
 let buttons = button.querySelectorAll("button");
 for (i = 0; i < 16; i++) {
     buttons[i].addEventListener("click", type);
 }
 let clear = document.querySelector(".clear");
-clear.addEventListener("click", function () {display.textContent = " ";})
+clear.addEventListener("click", function () {display.textContent = " "; operand1 = ""; operand2 = "", operator = ""})
 
 function type() {
+    
+    if (this.innerHTML=="*" || this.innerHTML=="+" || this.innerHTML=="/" || this.innerHTML=="-") {
+        operand1 = display.textContent;
+        operator = this.innerHTML;
+        display3.textContent = this.innerHTML;
+        display2.textContent = display.textContent;
+        display.textContent = "";
+    }
+    if (this.innerHTML == "=") {
+        operand2 = display.textContent;
+        display2.innerHTML = "";
+        display3.innerHTML = "";
+        console.log(operand2);
+        display.innerHTML=(operate(operand1, operator, operand2));
+        return;
+    }
     let now = this.innerHTML;
     display.textContent = display.textContent + now;
+    if (this.innerHTML=="*" || this.innerHTML=="+" || this.innerHTML=="/" || this.innerHTML=="-") {
+        display.textContent = "";
+    }
+    console.log(display);
+    console.log(operand1);
+    console.log(operator);
 }
 
 
 function add(num1, num2) {
+    num1 *= 1;
+    num2 *= 1;
     return num1 + num2;
 }
 function subtract(num1, num2) {
